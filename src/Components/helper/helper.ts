11f9f12d78392fc3dbd0d551ccbcce862207cadb
Danyyal ali <danyyal.ali.31@gmail.com>
2023-08-18 19:00:47 +0500

    changed the variables and improved code
    Signed-off-by: Danyyal ali <danyyal.ali.31@gmail.com>

--- src/Components/helper/helper.ts
+++ src/Components/helper/helper.ts
@@ -1,14 +1,14 @@
 export const getErrorMessage = (
-  min: number,
-  max: number,
-  target: number
+  minTemperature: number,
+  maxTemperature: number,
+  targetTemperature: number
 ): string | null => {
-  if (min > max)
-    return "Min temperature cannot be greater than max temperature";
-  else if (min > target)
-    return "Min temperature cannot be greater than target temperature";
-  else if (target > max)
-    return "Target temperature cannot be greater than max temperature";
+  if (minTemperature > maxTemperature)
+    return `Min temperature (${minTemperature}) cannot be greater than max temperature (${maxTemperature})`;
+  else if (minTemperature > targetTemperature)
+    return `Min temperature (${minTemperature}) cannot be greater than target temperature (${targetTemperature})`;
+  else if (targetTemperature > maxTemperature)
+    return `Target temperature (${targetTemperature}) cannot be greater than max temperature (${maxTemperature})`;
   return null;
 };
 export const getKnobPosition = (
@@ -17,8 +17,8 @@ export const getKnobPosition = (
   target: number
 ): number => {
   const tempDiff = max - min;
-  const normalizedTargetTemperature = target - min;
-  const percentage = (normalizedTargetTemperature * 100) / tempDiff;
-  const rotation = (280 * percentage) / 100;
-  return rotation + 40;
+  const normalizedTarget = target - min;
+  const percentage = (normalizedTarget * 100) / tempDiff;
+  const position = (280 * percentage) / 100;
+  return position + 40;
 };
